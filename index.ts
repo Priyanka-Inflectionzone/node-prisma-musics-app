fa4624b463c28981912cad03f428ad84897ac140
Priyanka-Inflectionzone <priyanka.kale@inflectionzone.com>
2022-10-01 18:41:18 +0530

    initialize prisma

---
+++ index.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+async function main() {
+    await prisma.UserInfo.create({
+       data:
+           {firstName : 'Priyanka',
+           lastName  : 'Kale',
+           email     : 'priyanka@ymail.com',
+           phone     : '9865741235',
+           createdAt : new Date('2020-10-11')} 
+        
+
+})
+
+}
+
+main()
+
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
